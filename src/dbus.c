b368a0930e821fc07a3e5b82e3df0cf08235c4f0
Dick Butt <dick@butt.com>
2024-07-16 00:32:14 +0300

    msg

--- src/dbus.c
+++ src/dbus.c
@@ -1,4 +1,4 @@
-/* copyright 2013 Sascha Kruse and contributors (see LICENSE for licensing information) */
+/* copyright 2013 Sascha Kruse and contributors (see LICENSE for licensing information)  111 */
 #include "dbus.h"
 
 #include <gio/gio.h>
